6001ca59ab0ab9424206644a39e45769d7b6f019
NguyenDucVinh-18 <vnguyenduc187@gmail.com>
2024-12-31 10:56:21 +0700

    first commit

---
+++ src/test/java/vn/edu/iuh/fit/laptopshop/LaptopShopApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.laptopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
