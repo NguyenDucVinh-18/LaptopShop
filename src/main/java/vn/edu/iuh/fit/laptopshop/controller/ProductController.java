614fa56fb18c9e779cfa88076bbb78efa69eb01d
NguyenDucVinh-18 <vnguyenduc187@gmail.com>
2025-02-13 18:58:07 +0700

    layout

---
+++ src/main/java/vn/edu/iuh/fit/laptopshop/controller/ProductController.java
@@ -0,0 +1,14 @@
+package vn.edu.iuh.fit.laptopshop.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+
+    @GetMapping("/admin/product")
+    public String getDashBoard() {
+        return "/admin/product/show";
+    }
+}
